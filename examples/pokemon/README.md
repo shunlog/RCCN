2eca2754cefd4913bcfc38bdd6ac9ee79f87f71c
shunlog <artiombalan331@gmail.com>
2023-06-04 18:37:34 +0300

    draft pokeapi example

---
+++ examples/pokemon/README.md
@@ -0,0 +1,1 @@
+This example is using a [Pokemon API](https://pokeapi.co/) through [this wrapper](https://github.com/beastmatser/aiopokeapi).
---
+++ examples/pokemon/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import sys
+import json
+
+from icecream import ic
+import asyncio
+import aiopoke
+
+from rccn import rccn
+
+async def fetch():
+    async with aiopoke.AiopokeClient() as client:
+       pok = await client.get_pokemon(1)
+
+    return pok
+
+def main(argv):
+    root = rccn.parse(argv[1])
+    ic(root)
+    for o in root.selection:
+        ic(o)
+
+    # resp = rccn.execute(root, swapi.resolve)
+    # print(json.dumps(resp, indent=4))
+
+    pok = asyncio.run(fetch())
+    ic(pok.name)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
