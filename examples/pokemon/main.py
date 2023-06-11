1405a36fdfefe60dc1111e869109841ac5769b38
shunlog <artiombalan331@gmail.com>
2023-06-11 23:08:10 +0300

    add pokemon example

--- examples/pokemon/main.py
+++ examples/pokemon/main.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python3
 import sys
 import json
-
-from icecream import ic
 import asyncio
+from icecream import ic
+from antlr4 import FileStream, InputStream
 import aiopoke
 
 from RCCN import rccn
 
-async def fetch():
-    async with aiopoke.AiopokeClient() as client:
-       pok = await client.get_pokemon(1)
 
-    return pok
+async def fetch(resource_name, id):
+    ic(resource_name)
+    async with aiopoke.AiopokeClient() as client:
+        method = getattr(client, 'get_' + resource_name)
+        res = await method(id)
+    return res
+
+
+# this resolver assumes that the parameters are correct,
+# if the field isn't a property of the parent object,
+# it can be fetched from the server
+def resolve(parent_type_name, field_name, obj, args):
+    if obj and hasattr(obj, field_name):
+        res = getattr(obj, field_name)
+    elif obj and hasattr(obj, field_name) and hasattr(getattr(obj, field_name), 'id'):
+        id = getattr(obj, field_name).id
+        res = asyncio.run(fetch(field_name, id))
+    else:
+        id = args['id']
+        res = asyncio.run(fetch(field_name, id))
+    return res
+
 
 def main(argv):
-    root = rccn.parse(argv[1])
-    ic(root)
-    for o in root.selection:
-        ic(o)
-
-    # resp = rccn.execute(root, swapi.resolve)
-    # print(json.dumps(resp, indent=4))
-
-    pok = asyncio.run(fetch())
-    ic(pok.name)
+    with open('typedefs.rccn', 'r') as file_defs:
+        with open(argv[1], 'r') as file_query:
+            input_stream = InputStream(file_defs.read() + file_query.read())
+    AST = rccn.parse(input_stream)
+    res = rccn.execute(AST, resolve)
+    ic(res)
 
 
 if __name__ == '__main__':
