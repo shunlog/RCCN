15c6864f0790c9cae21a79f61a0968750de586cf
shunlog <artiombalan331@gmail.com>
2023-06-13 19:05:12 +0300

    allow cors

--- examples/pokemon/server.py
+++ examples/pokemon/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from main import execute_query
 
@@ -9,6 +10,14 @@ class Query(BaseModel):
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/")
 async def query(data: Query):
